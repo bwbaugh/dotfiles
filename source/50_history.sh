6f9ba7e30339dd93fbb285f36df8d115e9a41b62
Wesley Baugh <wesley@bwbaugh.com>
2014-12-31 15:55:02 -0800

    Import bashrc by splitting into multiple source

---
+++ source/50_history.sh
@@ -0,0 +1,16 @@
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth:erasedups
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=100000
+HISTFILESIZE=100000
+
+# timestamp
+HISTTIMEFORMAT="+%FT%T "
+
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
