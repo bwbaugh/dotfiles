24dd5a64192f7855318b0d050f8e1954f9a603e9
Wesley Baugh <EMAIL>
2014-12-31 16:48:04 -0800

    Import installed homebrew items

---
+++ init/30_osx_homebrew_recipes.sh
@@ -0,0 +1,16 @@
+# OSX-only stuff. Abort if not OSX.
+is_osx || return 1
+
+# Exit if Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && e_error "Brew recipes need Homebrew to install." && return 1
+
+# Homebrew recipes
+recipes=(
+  bison
+  dialog
+  jq
+  openssl
+  watch
+)
+
+brew_install_recipes
