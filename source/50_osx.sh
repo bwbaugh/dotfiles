f8dd4108d22606ad8705daae7a3984c3285a3c4d
Wesley Baugh <wesley@bwbaugh.com>
2015-05-24 11:24:23 -0700

    Place binaries from Haskell Cabal on the PATH

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -4,4 +4,4 @@ is_osx || return 1
 test -f ~/.git-completion.bash && . $_
 
 # Adapt the PATH environment variable for use with MacPorts.
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+export PATH=/Users/bwbaugh/.cabal/bin:/opt/local/bin:/opt/local/sbin:$PATH
