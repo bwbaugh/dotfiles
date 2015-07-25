c3868e7d2e4af81ef684d1da39b81121673475d2
Wesley Baugh <wesley@bwbaugh.com>
2015-07-24 21:01:30 -0700

    Define helper functions for adding to the path

---
+++ source/01_path.sh
@@ -0,0 +1,17 @@
+# Source: http://superuser.com/a/39995/215067
+#
+# Add a directory to $PATH if it's not aleady there.
+
+
+pathaddstart() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        PATH="$1${PATH:+":$PATH"}"
+    fi
+}
+
+
+pathaddend() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        PATH="${PATH:+"$PATH:"}$1"
+    fi
+}
