7074ebd719c5f766e8057235d8b91f2014160915
Wesley Baugh <wesley@bwbaugh.com>
2016-02-15 18:03:58 -0800

    Add path for linux

--- init/40_powerline.sh
+++ init/40_powerline.sh
@@ -2,7 +2,11 @@ PIP_REQUIRE_VIRTUALENV=false pip install --user powerline-status psutil
 
 readonly conf_file="$HOME/.tmux-powerline.conf"
 
-readonly py27_powerline="$HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/tmux/powerline.conf"
-if [ -f "${py27_powerline}" ] ; then
-    ln -s -f "${py27_powerline}" "${conf_file}"
+readonly py27_osx_powerline="$HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/tmux/powerline.conf"
+if [ -f "${py27_osx_powerline}" ] ; then
+    ln -s -f "${py27_osx_powerline}" "${conf_file}"
+fi
+readonly py27_linux_powerline="$HOME/.local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf"
+if [ -f "${py27_linux_powerline}" ] ; then
+    ln -s -f "${py27_linux_powerline}" "${conf_file}"
 fi
