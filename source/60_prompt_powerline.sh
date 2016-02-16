83ae86e4a957dd0c33889ea297153c1e334db22d
Wesley Baugh <wesley@bwbaugh.com>
2016-02-15 20:29:13 -0800

    Bash prompt using Powerline
    Need the left theme since right segments don't show.
    
    https://github.com/powerline/powerline/issues/1149

---
+++ source/60_prompt_powerline.sh
@@ -0,0 +1,12 @@
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+
+readonly py27_osx_powerline="$HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh"
+if [ -f "${py27_osx_powerline}" ] ; then
+    source "${py27_osx_powerline}"
+fi
+readonly py27_linux_powerline="$HOME/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh"
+if [ -f "${py27_linux_powerline}" ] ; then
+    source "${py27_linux_powerline}"
+fi
