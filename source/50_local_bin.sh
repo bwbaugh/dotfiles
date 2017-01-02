62a82493ed3a3b05d4a08d7cbf79332dfda5a303
Wesley Baugh <bwbaugh@gmail.com>
2017-01-02 17:01:53 -0500

    Add top-level home bin since sometimes needed

--- source/50_local_bin.sh
+++ source/50_local_bin.sh
@@ -3,3 +3,9 @@ pathaddstart "$HOME/.local/bin"
 
 # Installed to home directory.
 pathaddstart "$HOME/opt/bin"
+
+# Personal bin.
+# XXX(2016-01-02): This seems to be added to the path for the first,
+#   say, tmux pane, by something else, but later panes don't have it.
+#   Adding it here so that the directory is always on the path.
+pathaddstart "$HOME/bin"
